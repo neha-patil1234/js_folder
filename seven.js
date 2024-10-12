8fc6e38292ccc9ff1c0ecd6b8906e4e06e96ff8c
nehapatil <nehapatil10022004@gmail.com>
2024-10-12 17:42:26 +0530

    first commit

---
+++ seven.js
@@ -0,0 +1,52 @@
+// let div=document.querySelector("div");
+// console.log(div);
+
+// let id= div.getAttribute("id");
+// console.log(id);
+
+// let name= div.getAttribute("name");
+// console.log(name);
+
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+// console.log(para.getAttribute("class","newClass"));
+
+
+
+// let  div=document.querySelector("div");
+// div.style.backgroundColor="green";
+// div.style.visibility="hidden";
+
+// div.style.fotnSize="26px";
+// div.innerText="hellow";
+// let newBtn=document.createElement("button");
+// newBtn.innerText="clik me";
+// console.log(newBtn);
+
+// let div=document.querySelector("div");
+// div.after(newBtn);
+
+// let newHeading=document.createElement("h1");
+// newHeading.innerHYML="<i>hi,i am new!</i>";
+// document.querySelector("body").prepend(newHeading);
+// let para=document.querySelector("p");
+// para.remove();
+
+
+
+
+
+
+//q1:
+let newBtn=document.createElement("button");
+newBtn.innerText="click me!"
+
+newBtn.style.color="white";
+newBtn.style.backgroundColor="red";
+
+document.querySelector("body").prepend(newBtn);
+
+
+//q2:
+let para=document.querySelector("p");
+
